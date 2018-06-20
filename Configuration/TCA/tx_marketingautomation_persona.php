b3cf073b17d381f6d9788282ab025d6d3906ab47
Nicole Cordes <nicole@biz-design.biz>
2018-06-20 11:35:39 +0200

    Add basic database table

---
+++ Configuration/TCA/tx_marketingautomation_persona.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:marketing_automation/Resources/Private/Language/locallang_tca.xlf:tx_marketingautomation_persona',
+        'descriptionColumn' => 'description',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'delete' => 'deleted',
+        'sortby' => 'sorting',
+        'searchFields' => 'title,description',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'typeicon_classes' => [
+            'default' => 'mimetypes-x-tx_marketingautomation_persona',
+        ],
+    ],
+    'interface' => [
+        'showRecordFieldList' => 'title,description',
+    ],
+    'types' => [
+        '1' => [
+            'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,'
+                . 'title,'
+                . '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,'
+                . 'hidden,--palette--;;timeRestriction,'
+                . '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,'
+                . 'description,'
+                . '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+            ',
+        ],
+    ],
+    'palettes' => [
+        'timeRestriction' => [
+            'showitem' => 'starttime, endtime',
+        ],
+    ],
+    'columns' => [
+        'hidden' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
+            'config' => [
+                'type' => 'check',
+            ],
+        ],
+        'starttime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime,int',
+                'default' => 0,
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ],
+        ],
+        'endtime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime,int',
+                'default' => 0,
+                'range' => [
+                    'upper' => mktime(0, 0, 0, 1, 1, 2038),
+                ],
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ],
+        ],
+        'title' => [
+            'label' => 'LLL:EXT:marketing_automation/Resources/Private/Language/locallang_tca.xlf:tx_marketingautomation_persona.title',
+            'config' => [
+                'type' => 'input',
+                'width' => 200,
+                'eval' => 'trim,required',
+            ],
+        ],
+        'description' => [
+            'label' => 'LLL:EXT:marketing_automation/Resources/Private/Language/locallang_tca.xlf:tx_marketingautomation_persona.description',
+            'config' => [
+                'type' => 'text',
+                'default' => '',
+            ],
+        ],
+    ],
+];
---
+++ ext_tables.php
@@ -0,0 +1,15 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+call_user_func(function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_marketingautomation_persona');
+
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'mimetypes-x-tx_marketingautomation_persona',
+        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        [
+            'source' => 'EXT:marketing_automation/Resources/Public/Icons/tx_marketingautomation_persona.svg',
+        ]
+    );
+});
