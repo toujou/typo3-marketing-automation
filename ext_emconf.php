16095c324a9df3a269cd739ac7c9686fe00a4f5b
Nicole Cordes <nicole@biz-design.biz>
2018-06-20 10:42:54 +0200

    Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "marketing_automation".
+ *
+ * Auto generated 20-06-2018 10:41
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Marketing Automation',
+  'description' => '',
+  'category' => 'fe',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"0741";s:36:"Resources/Public/Icons/Extension.svg";s:4:"4473";}',
+);
+
