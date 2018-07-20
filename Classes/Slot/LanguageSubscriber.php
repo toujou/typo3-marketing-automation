9c7c3c9619a361d43b0dce246e4256c67b7a30b7
Nicole Cordes <nicole@biz-design.biz>
2018-07-20 18:31:21 +0200

    [BUGFIX] Decouple language handling from TypoScriptFrontendController

--- Classes/Slot/LanguageSubscriber.php
+++ Classes/Slot/LanguageSubscriber.php
@@ -4,29 +4,54 @@ namespace Bitmotion\MarketingAutomation\Slot;
 
 use Bitmotion\MarketingAutomation\Dispatcher\SubscriberInterface;
 use Bitmotion\MarketingAutomation\Persona\Persona;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class LanguageSubscriber implements SubscriberInterface
 {
     /**
-     * @var TypoScriptFrontendController
+     * @var int
      */
-    protected $typoScriptFrontendController;
+    protected $languageId;
 
-    public function __construct(TypoScriptFrontendController $typoScriptFrontendController = null)
+    public function __construct()
     {
-        $this->typoScriptFrontendController = $typoScriptFrontendController ?: $GLOBALS['TSFE'];
+        $this->languageId = (int)GeneralUtility::_GP('L');
     }
 
     public function needsUpdate(Persona $currentPersona, Persona $newPersona): bool
     {
-        $language = $newPersona->getLanguage();
+        if (!$this->isValidLanguageId()) {
+            $this->languageId = 0;
+        }
 
-        return $this->typoScriptFrontendController->sys_language_uid !== $language;
+        return $this->languageId !== $newPersona->getLanguage();
     }
 
     public function update(Persona $persona): Persona
     {
-        return $persona->withLanguage($this->typoScriptFrontendController->sys_language_uid);
+        return $persona->withLanguage($this->languageId);
+    }
+
+    protected function isValidLanguageId()
+    {
+        if ($this->languageId === 0) {
+            return true;
+        }
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_language');
+
+        $count = $queryBuilder->count('uid')
+                ->from('sys_language')
+                ->where(
+                    $queryBuilder->expr()->eq(
+                        'uid',
+                        $queryBuilder->createNamedParameter($this->languageId, \PDO::PARAM_INT)
+                    )
+                )
+                ->execute()
+                ->fetchColumn();
+
+        return $count === 1;
     }
 }
