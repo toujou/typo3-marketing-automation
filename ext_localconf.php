dcdfc88200038645cdf2508ba42a517e416b4ef2
Nicole Cordes <nicole@biz-design.biz>
2018-07-20 18:17:14 +0200

    [BUGFIX] Move dispatcher execution before getFromCache is called

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['settingLanguage_postProcess']['marketing_automation'] =
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PreProcessing']['marketing_automation'] =
         \Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class . '->dispatch';
 
     $marketingDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class);
