c0bc449d879b87e846e8331604833ba7d16cd47b
Florian Wessels <f.wessels@bitmotion.de>
2018-11-21 13:51:03 +0100

    [TASK] Add author information to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,28 +10,24 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-  'title' => 'Marketing Automation',
-  'description' => 'Base TYPO3 extension that allows Targeting and Personalization of TYPO3 content: Limit pages, content elements etc. to certain "Personas". Determination of Personas can come from various sources (requires add-on extensions).',
-  'category' => 'fe',
-  'state' => 'stable',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 0,
-  'version' => '1.0.0',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '8.7.0-8.7.99',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"0741";s:36:"Resources/Public/Icons/Extension.svg";s:4:"4473";}',
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Marketing Automation',
+    'description' => 'Base TYPO3 extension that allows Targeting and Personalization of TYPO3 content: Limit pages, content elements etc. to certain "Personas". Determination of Personas can come from various sources (requires add-on extensions).',
+    'category' => 'fe',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.1-dev',
+    'author' => 'Bitmotion GmbH',
+    'author_company' => 'Bitmotion GmbH',
+    'author_email' => 'typo3-ext@bitmotion.de',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
 
