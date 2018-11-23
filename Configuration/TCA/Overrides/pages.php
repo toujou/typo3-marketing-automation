a9be64125c6140c231ae7de7de6fa411e67d7f54
Florian Wessels <f.wessels@bitmotion.de>
2018-11-23 13:17:50 +0100

    [TASK] Seperate TCA override files

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,5 +1,4 @@
 <?php
 declare(strict_types = 1);
 
-$GLOBALS['TCA']['tt_content']['ctrl']['enablecolumns'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::PERSONA_ENABLE_FIELDS_KEY] = 'tx_marketingautomation_persona';
 $GLOBALS['TCA']['pages']['ctrl']['enablecolumns'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::PERSONA_ENABLE_FIELDS_KEY] = 'tx_marketingautomation_persona';
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types = 1);
+
+$GLOBALS['TCA']['tt_content']['ctrl']['enablecolumns'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::PERSONA_ENABLE_FIELDS_KEY] = 'tx_marketingautomation_persona';
