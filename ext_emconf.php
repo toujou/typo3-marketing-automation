74c62b30cce9a1f8713414a36e1c18e843900bf3
Florian Wessels <f.wessels@bitmotion.de>
2020-04-21 22:07:00 +0200

    [TASK] Set version to 1.2.0-dev and adapt author information

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,17 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "marketing_automation".
- *
- * Auto generated 20-06-2018 10:41
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['marketing_automation'] = [
     'title' => 'Marketing Automation',
-    'description' => 'Base TYPO3 extension that allows Targeting and Personalization of TYPO3 content: Limit pages, content elements etc. to certain "Personas". Determination of Personas can come from various sources (requires add-on extensions).',
+    'description' => 'Base TYPO3 extension that allows targeting and personalization of TYPO3 content: Limit pages, content elements etc. to certain "Personas". Determination of Personas can come from various sources (requires add-on extensions).',
     'category' => 'fe',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => false,
-    'version' => '1.1.0',
-    'author' => 'Bitmotion GmbH',
-    'author_company' => 'Bitmotion GmbH',
-    'author_email' => 'typo3-ext@bitmotion.de',
+    'version' => '1.2.0-dev',
+    'author' => 'Florian Wessels, Helmut Hummbel',
+    'author_company' => 'Leuchtfeuer Digital Marketing',
+    'author_email' => 'dev@Leuchtfeuer.com',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
