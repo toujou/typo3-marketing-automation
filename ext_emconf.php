9c0844a9d7f9f30ac9e1472b7cc936f20cb817bf
Florian Wessels <f.wessels@bitmotion.de>
2020-04-21 22:06:32 +0200

    [TASK] Drop TYPO3 v8 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3-ext@bitmotion.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
