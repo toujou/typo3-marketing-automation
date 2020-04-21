f3ede7a2aba6a17b2f435c980b90d7aae62197c4
Florian Wessels <f.wessels@bitmotion.de>
2020-04-21 22:07:41 +0200

    [TASK] Use extension key as argument

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,45 +1,47 @@
 <?php
 defined('TYPO3_MODE') || die;
 
-call_user_func(function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PreProcessing']['marketing_automation'] =
-        \Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class . '->dispatch';
+call_user_func(
+    function ($extensionKey) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PreProcessing']['marketing_automation'] =
+            \Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class . '->dispatch';
 
-    $marketingDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class);
-    $marketingDispatcher->addSubscriber(\Bitmotion\MarketingAutomation\Slot\LanguageSubscriber::class);
-    $marketingDispatcher->addListener(\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class . '->fetchCurrentPersona');
+        $marketingDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Bitmotion\MarketingAutomation\Dispatcher\Dispatcher::class);
+        $marketingDispatcher->addSubscriber(\Bitmotion\MarketingAutomation\Slot\LanguageSubscriber::class);
+        $marketingDispatcher->addListener(\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class . '->fetchCurrentPersona');
 
-    if (!isset($GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class])) {
-        $GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = [];
-    }
+        if (!isset($GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class])) {
+            $GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = [];
+        }
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class . '->addPersonaToCacheIdentifier';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \Bitmotion\MarketingAutomation\Hook\BackendIconOverlayHook::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class . '->addPersonaToCacheIdentifier';
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Imaging\IconFactory::class]['overrideIconOverlay'][] = \Bitmotion\MarketingAutomation\Hook\BackendIconOverlayHook::class;
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawFooter'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawFooter'][\Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class] = \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class;
 
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 
-    if (class_exists('\TYPO3\CMS\Install\Service\SqlExpectedSchemaService')) {
-        $signalClass = \TYPO3\CMS\Install\Service\SqlExpectedSchemaService::class;
-    } else {
-        /**
-         * TODO: Use SqlReader::class in future
-         * @see https://forge.typo3.org/issues/86851
-         */
-        $signalClass = 'TYPO3\\CMS\\Install\\Service\\SqlExpectedSchemaService';
-    }
+        if (class_exists('\TYPO3\CMS\Install\Service\SqlExpectedSchemaService')) {
+            $signalClass = \TYPO3\CMS\Install\Service\SqlExpectedSchemaService::class;
+        } else {
+            /**
+             * TODO: Use SqlReader::class in future
+             * @see https://forge.typo3.org/issues/86851
+             */
+            $signalClass = 'TYPO3\\CMS\\Install\\Service\\SqlExpectedSchemaService';
+        }
 
-    $signalSlotDispatcher->connect(
-        $signalClass,
-        'tablesDefinitionIsBeingBuilt',
-        \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class,
-        'getPersonaFieldsRequiredDatabaseSchema'
-    );
-    $signalSlotDispatcher->connect(
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::class,
-        'tcaIsBeingBuilt',
-        \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class,
-        'addPersonaRestrictionFieldToTca'
-    );
-});
+        $signalSlotDispatcher->connect(
+            $signalClass,
+            'tablesDefinitionIsBeingBuilt',
+            \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class,
+            'getPersonaFieldsRequiredDatabaseSchema'
+        );
+        $signalSlotDispatcher->connect(
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::class,
+            'tcaIsBeingBuilt',
+            \Bitmotion\MarketingAutomation\Persona\PersonaRestriction::class,
+            'addPersonaRestrictionFieldToTca'
+        );
+    }, 'marketing_automation'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,22 +1,26 @@
 <?php
 defined('TYPO3_MODE') || die;
 
-call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_marketingautomation_persona');
+call_user_func(
+    function ($extensionKey) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_marketingautomation_persona');
 
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'mimetypes-x-tx_marketingautomation_persona',
-        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        [
-            'source' => 'EXT:marketing_automation/Resources/Public/Icons/tx_marketingautomation_persona.svg',
-        ]
-    );
-    $iconRegistry->registerIcon(
-        'overlay-frontendusers-tx_marketingautomation_persona',
-        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        [
-            'source' => 'EXT:marketing_automation/Resources/Public/Icons/overlay-personas.svg',
-        ]
-    );
-});
+        // Register some icons
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'mimetypes-x-tx_marketingautomation_persona',
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => 'EXT:marketing_automation/Resources/Public/Icons/tx_marketingautomation_persona.svg',
+            ]
+        );
+
+        $iconRegistry->registerIcon(
+            'overlay-frontendusers-tx_marketingautomation_persona',
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => 'EXT:marketing_automation/Resources/Public/Icons/overlay-personas.svg',
+            ]
+        );
+    }, 'marketing_automation'
+);
